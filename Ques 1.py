41a52af943abbe61fec3fd0fbf4fc7195cfa5e3c
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-06-12 16:41:36 +0530

    Add files via upload

---
+++ Ques 1.py
@@ -0,0 +1,15 @@
+m=int(input("Enter Marks :"))
+if(m<25):
+    print(" Grade F ")
+elif(m>=25 and m<45):
+    print(" Grade E ")
+elif(m>=45 and m<50):
+    print(" Grade D ")
+elif(m>=50 and m<60):
+    print(" Grade C ")
+elif(m>=60 and m<80):
+    print(" Grade B ")
+elif(m>=80):   
+    print(" Grade A ")
+else:
+    print(" No result ")
